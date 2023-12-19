4830c0867af523e4d2aa877a6c7aee38cb940625
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-19 03:35:25 -0300

    07-Bio page
    07-Bio page

---
+++ src/app/bio/page.js
@@ -0,0 +1,27 @@
+
+
+export default async function BioPage(){
+    const request = await fetch('https://baconipsum.com/api/?type=all-meat&para=5');
+    const bioData = await request.json()
+
+    return(
+        <div className="row">
+            <article>
+                <div className="mb-4">
+                    <h1 className="fw-bolder mb-1">
+                        Hokusai Bio
+                    </h1>
+                </div>
+                <section className="mb-5">
+                    {bioData.map((bio,i)=>(
+                        <p key={i} className="fs-5 mb-4">
+                            {bio}
+                        </p>
+                    ))}
+                </section>
+            </article>
+        </div>
+    )
+
+
+}
