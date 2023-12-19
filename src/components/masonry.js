35e27d92a5db3c5eb86a706b6c2037fc431c1533
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-19 02:52:22 -0300

    05-Showing home articles
    05-Showing home articles

--- src/components/masonry.js
+++ src/components/masonry.js
@@ -1,12 +1,43 @@
 "use client"
+import Masonry from 'react-masonry-css'
+import Button from 'react-bootstrap/Button'
+import Card  from 'react-bootstrap/Card';
+import Link from  'next/navigation'
+import { Fade } from 'react-awesome-reveal'
 
 export default function MasonryComponent({data}){
-
-    console.log(data)
-
+    const breakpoints = {
+        default: 3,
+        700: 2,
+        500: 1
+      };
+  
     return(
         <>
-            masonry
+            <Masonry
+                breakpointCols={breakpoints}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
+                { data.map((article)=>(
+                <Fade key={article.id}>
+                    <Card style={{width:'100%'}}>
+                        <Card.Img variant='top' src={`/images/arts/${article.img}`}/>
+                        <Card.Body>
+                            <Card.Title>{article.name}</Card.Title>
+                            <Card.Text>{article.excerpt}</Card.Text>
+                            <Button
+                                variant='outline-primary'
+                                as={Link}
+                                href={`/${article.id}`}
+                            >
+                                See article
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Fade>
+                ))}
+            </Masonry>
         </>
     )
 }
