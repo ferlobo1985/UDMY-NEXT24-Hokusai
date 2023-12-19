a872f34a0ea43959becc63a8e50c89ece0fc2e51
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-12-19 03:18:39 -0300

    06-Article by ID
    06-Article by ID

---
+++ src/app/[id]/page.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import { getArticleById } from '../../utils/actions'
+
+export default async function ArticlePage({params}){
+    const {id} = params;
+    const article = await getArticleById(id)
+
+    return(
+        <div>
+            <div
+                style={{
+                    marginTop:'20px',
+                    position:'relative',
+                    width:'100%',
+                    height:'500px'
+                }}
+            >
+                <Image 
+                    src={`/images/arts/${article.img}`}
+                    alt={article.name}
+                    fill
+                    style={{objectFit:"cover"}}
+                />
+            </div>
+            <div className='article_container'>
+                <h1>{article.name}</h1>
+                <div dangerouslySetInnerHTML={{__html:article.content}}></div>
+            </div>
+        </div>
+    )
+
+}
--- src/utils/actions.js
+++ src/utils/actions.js
@@ -13,5 +13,20 @@ export async function getArticles(){
     } catch(error){
         throw new Error(error.message)
     }
+}
 
+
+export async function getArticleById(id){
+    try{
+        const getArticle = await fetch(`http://localhost:3004/articles/${id}`);
+        const article = await getArticle.json();
+
+        if(Object.keys(article).length === 0){
+            throw new Error('We could not find the articles')
+        }
+
+        return article;
+    } catch(error){
+        throw new Error(error.message)
+    }
 }
